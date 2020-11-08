081b905219945d7641f8d6cef965050935ff9746
Aditi Garg <aditigargbti09@gmail.com>
2020-11-08 18:02:21 +0530

    Create list_smallestElement.py

---
+++ list_smallestElement.py
@@ -0,0 +1,9 @@
+num=int(input("Enter number of elements you want to enter in a list: "))
+list1=[]
+
+print("Now enter",num,"elements")
+for i in range(0,num):
+    ele=int(input())
+    list1.append(ele)
+
+print("Smallest element in list:",min(list1))
