b77e8e610ce84789e5a830c0b9dde2a2fc6d6656
Aditi Garg <aditigargbti09@gmail.com>
2020-11-08 18:51:54 +0530

    Update list.py

--- list.py
+++ list.py
@@ -3,7 +3,6 @@ Write a Python program to count the number of strings where the string length is
 Sample List : ['abc', 'xyz', 'aba', '1221']
 Expected Result : 2
 Question taken from https://www.w3resource.com/python-exercises/list/#EDITOR
-
 '''
 
 num=int(input("Enter number of elements you want to enter in a list: "))
@@ -15,11 +14,9 @@ for i in range(0,num):
     list1.append(ele)
 
 count=0
-
 for i in range(0,num):
     ele=list1[i]
     rev=ele[::-1]
     if list1[i]==rev:
-        count+=1
-      
+        count+=1   
 print(count)
