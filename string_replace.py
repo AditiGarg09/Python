19370ec438efa6ef712a1dab1bdb291043186921
Aditi Garg <aditigargbti09@gmail.com>
2020-11-17 13:49:02 +0530

    Update string_replace.py

--- string_replace.py
+++ string_replace.py
@@ -3,11 +3,11 @@
 string=input("Enter String: ")
 result=string[0]
 flag=0
+
 for i in range(1,len(string)):
     for j in result:
         if string[i]!=j:
-            flag=1
-            
+            flag=1 
         else:
             flag=0
             break
